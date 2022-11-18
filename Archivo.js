064b9ae3abdb16e7eadf63ca91aa6eb7163f4634
Eillou <97463387+Eillou@users.noreply.github.com>
2022-11-18 09:43:00 -0600

    Commit
    Esta es la clase en la que aprendiste por primera vez JavaScript

---
+++ Archivo.js
@@ -0,0 +1,58 @@
+//alert("Do you want cookies?"); //Esto es un comentario;
+var number = 10;
+var number1 = 30;
+
+//alert(number + number1);
+
+var number_letras ="veinte";
+var number_letras2 ="uno";
+
+//alert(number_letras + " " + number_letras2);
+
+var bool = number>number1;
+//alert(bool);
+
+//Operadores == != > < >= <=;
+// AND && OR || NOT !;
+//Condicionales
+
+//if(bool ==true)
+//alert(number_letras)
+//else 
+//alert("El número es mayor que el ingresado");
+
+var letra ="O";
+
+//switch(letra){
+    //case 'a': alert("Letra A");
+    //break;
+    //case 'b': alert("Letra B");
+    //break;
+    //case 'c': alert("Letra C");
+    //break;
+    //default: alert("La letra no se encuentra en la base de datos");
+    //break;
+    //}
+
+var x=0;
+//while(x<11){
+    //alert(x);
+    //x++;
+    //}
+
+//do {
+    //alert(x);
+    //x++;
+//}while(x<4)
+
+//for(var z=0; z<=11; z++){
+    //alert("Estamos en: "+ z);
+//}
+
+//Funciones
+
+//function MiFuncion() {
+ //var name = document.getElementById('Nombre').value;
+ //alert ("Tu nombre es: " + name)
+//}
+
